41d6ebef6148d1a1a0b1ff58381eff37417c4d55
Cristian Lahoz <lahozcristian@gmail.com>
2023-11-05 23:11:38 -0300

    feat: :sparkles: add post, put & delete

--- users.py
+++ users.py
@@ -7,6 +7,7 @@ app = FastAPI()
 
 # Entidad User
 class User(BaseModel):
+    id: int
     name: str
     surname: str
     url: str
@@ -14,12 +15,79 @@ class User(BaseModel):
 
 
 users_list = [
-    User(name="Cristian", surname="Lahoz", url="url", age=37),
-    User(name="Cristian", surname="Lahoz", url="url", age=37),
-    User(name="Cristian", surname="Lahoz", url="url", age=37),
+    User(id=1, name="Cristian", surname="Lahoz", url="url", age=37),
+    User(id=2, name="Cristian", surname="Lahoz", url="url", age=37),
+    User(id=3, name="Cristian", surname="Lahoz", url="url", age=37),
+    User(id=4, name="Cristian", surname="Lahoz", url="url", age=37),
 ]
 
 
+# GET
 @app.get("/users")
 async def users():
     return users_list
+
+
+# Path
+@app.get("/user/{id}")
+async def user(id: int):  # type: ignore
+    return search_user(id)
+
+
+# Query
+@app.get("/user/")
+async def user(id: int):  # type: ignore
+    return search_user(id)
+
+
+# POST
+@app.post("/user/")
+async def user(user: User):  # type: ignore
+    if type(search_user(user.id)) == User:
+        return {"error": "El usuario ya existe"}
+
+    users_list.append(user)
+    return user
+
+
+# {"id":5, "name":"Cristian", "surname":"Lahoz", "url":"url", "age":37}
+
+
+# PUT
+@app.put("/user/")
+async def user(user: User):  # type: ignore
+    found = False
+
+    for index, saved_user in enumerate(users_list):
+        if saved_user.id == user.id:
+            users_list[index] = user
+            found = True
+
+    if not found:
+        return {"error": "No se ha actualizado usuario"}
+
+    return user
+
+
+# DELETE
+@app.delete("/user/{id}")
+async def user(id: int):
+    found = False
+
+    for index, saved_user in enumerate(users_list):
+        if saved_user.id == id:
+            del users_list[index]
+            found = True
+
+    if not found:
+        return {"error": "No se ha eliminado usuario"}
+
+
+# Search user
+def search_user(id: int):
+    users = filter(lambda user: user.id == id, users_list)
+
+    try:
+        return list(users)[0]
+    except:
+        return {"error": "No se ha encontrado usuario"}
