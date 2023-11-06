1e0ce83d540fe36ce0209698744c11db44c8fa3d
Cristian Lahoz <lahozcristian@gmail.com>
2023-11-06 19:34:17 -0300

    feat: :closed_lock_with_key: Add security oauth2

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # Instala FastAPI: pip install "fastapi[all]"
 
 from fastapi import FastAPI
-from routers import products, users
+from routers import products, users, basic_auth_users, jwt_auth_users
 from fastapi.staticfiles import StaticFiles
 
 
@@ -13,6 +13,8 @@ app = FastAPI()
 # Routers
 app.include_router(products.router)
 app.include_router(users.router)
+app.include_router(basic_auth_users.router)
+app.include_router(jwt_auth_users.router)
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
