aeae26783255bb8668ab951b9116f1899989facf
Cristian Lahoz <lahozcristian@gmail.com>
2023-11-07 19:09:10 -0300

    feat: :card_file_box: Deploying Database MongoDB

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # Instala FastAPI: pip install "fastapi[all]"
 
 from fastapi import FastAPI
-from routers import products, users, basic_auth_users, jwt_auth_users
+from routers import products, users, basic_auth_users, jwt_auth_users, users_db
 from fastapi.staticfiles import StaticFiles
 
 
@@ -15,6 +15,7 @@ app.include_router(products.router)
 app.include_router(users.router)
 app.include_router(basic_auth_users.router)
 app.include_router(jwt_auth_users.router)
+app.include_router(users_db.router)
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
